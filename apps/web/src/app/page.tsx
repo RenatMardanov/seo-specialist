bd48c220e32b1c5a5957d9868d6fe1f8890a9906
Renat Mardanov <rent1995@ya.ru>
2025-10-01 06:43:51 +0700

    initial:add  next15 + shadcn apps

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@seo-specialist/ui';
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.none file.
+   */
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ packages/web/ui/src/lib/ui.tsx
@@ -0,0 +1,9 @@
+export function SeoSpecialistUi() {
+  return (
+    <div>
+      <h1>Welcome to SeoSpecialistUi!</h1>
+    </div>
+  );
+}
+
+export default SeoSpecialistUi;
