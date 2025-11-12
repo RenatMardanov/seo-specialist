437013342fff2feb2f128fbd820cf2390365f094
Renat Mardanov <rent1995@ya.ru>
2025-11-12 22:02:23 +0700

    feat: add react/vite app

---
+++ apps/dashboard/src/app/app.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@seo-specialist/ui';
+
+export default function App() {
+  return (
+    <div>
+      <Button variant="outline" size="lg">
+        Click me
+      </Button>
+    </div>
+  );
+}
